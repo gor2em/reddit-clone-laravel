e82f169e0579303d5541f2077e1ca4948205db58
gor2em <grkem.squash99@gmail.com>
2022-09-03 17:59:02 +0300

    display post

--- resources/js/Components/PostCard.vue
+++ resources/js/Components/PostCard.vue
@@ -27,7 +27,7 @@ defineProps({
                     {{ post.created_at }}
                 </div>
             </div>
-            <!-- <Link
+            <Link
                 :href="
                     route('frontend.communities.posts.show', [
                         community,
@@ -36,23 +36,18 @@ defineProps({
                 "
             >
                 <h5
-                    class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white hover:text-indigo-700"
+                    class="mb-2 text-2xl font-bold tracking-tight text-gray-900  hover:text-indigo-700"
                 >
                     {{ post.title }}
                 </h5>
-            </Link> -->
+            </Link>
 
-            <h5
-                    class="mb-2 text-2xl font-bold tracking-tight text-gray-900 hover:text-indigo-700"
-                >
-                    {{ post.title }}
-                </h5>
             <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
                 {{ post.description }}
             </p>
             <div class="flex m-2 p-2">
                 <p class="mr-4 p-2">Comments({{ post.comments_count }})</p>
-                <!-- <Link
+                 <Link
                     :href="
                         route('frontend.communities.posts.show', [
                             community,
@@ -75,22 +70,7 @@ defineProps({
                             clip-rule="evenodd"
                         ></path>
                     </svg>
-                </Link> -->
-
-                Read more
-                    <svg
-                        aria-hidden="true"
-                        class="ml-2 -mr-1 w-4 h-4"
-                        fill="currentColor"
-                        viewBox="0 0 20 20"
-                        xmlns="http://www.w3.org/2000/svg"
-                    >
-                        <path
-                            fill-rule="evenodd"
-                            d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-                            clip-rule="evenodd"
-                        ></path>
-                    </svg>
+                </Link>
             </div>
         </div>
     </div>
---
+++ resources/js/Pages/Frontend/Posts/Show.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { Link } from "@inertiajs/inertia-vue3";
+import GuestLayout from "@/Layouts/Guest.vue";
+
+defineProps({
+    community: Object,
+    post: Object,
+});
+</script>
+
+<template>
+    <guest-layout>
+        <section class="m-2 p-2 flex md:flex-row">
+            <div class="w-8/12">
+                <h1>{{ post.title }}</h1>
+                Post Details
+            </div>
+            <div class="w-4/12 p-4">
+                <div class="m-2 p-2 bg-slate-600 text-white">
+                    <h2>Latest Communities</h2>
+                </div>
+            </div>
+        </section>
+    </guest-layout>
+</template>
